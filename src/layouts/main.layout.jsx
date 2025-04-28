44ee5c4964557537847c04e59026cb9ee585f520
Zafra Ziaudeen <zafraziaudeen@gmail.com>
2025-04-28 13:52:24 +0530

    modified main layout

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -1,14 +1,17 @@
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 function MainLayout() {
-    return (
-        <>
-            <Navigation  />
-            <Outlet />
-            <Footer/>
-        </>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navigation />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
+
 export default MainLayout;
